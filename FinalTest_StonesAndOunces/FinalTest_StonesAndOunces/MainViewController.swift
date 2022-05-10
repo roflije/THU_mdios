546f69d328a19289ed93e7f2f41f36aab883ced9
Schneider <paschneider@c05-mac-04eth.informatik.hs-ulm.de>
2022-05-10 18:48:22 +0200

    table view

--- FinalTest_StonesAndOunces/FinalTest_StonesAndOunces/MainViewController.swift
+++ FinalTest_StonesAndOunces/FinalTest_StonesAndOunces/MainViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MainViewController: UIViewController 	 {
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate	 {
     var people = [Person]()
     var data = [String]()
     @IBOutlet weak var tv: UITableView!
@@ -44,6 +44,7 @@ class MainViewController: UIViewController 	 {
         for p in people {
             print(p.toString())
         }
+        tv.reloadData()
     }
     
     func getXML() -> Void {
@@ -52,13 +53,31 @@ class MainViewController: UIViewController 	 {
         people = customParser.getPeople()
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.people.count
+    }
     
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let label = UILabel(frame: CGRect(x:0, y:12.5, width: 500, height:50))
+        label.text = people[indexPath[1]].name + ", " + String(format: "%.2f", people[indexPath[1]].getKG()) + " kg"
+        label.sizeToFit()
+        cell.addSubview(label)
+        return cell
+    }
+    
+    private func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+            return 300
+        }
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hi")
         segmentLoad()
+        tv.dataSource = self
+        tv.delegate = self
+        
         print("loaded!")
-        // Do any additional setup after loading the view.
     }
 }
 
